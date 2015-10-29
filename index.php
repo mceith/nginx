91f69d9c3e84b8f51938cf96e2d21fc501065868
mceith <contact@mceith.com>
2015-10-29 13:14:53 +0000

    fixed typos

--- index.php
+++ index.php
@@ -9,5 +9,5 @@
    $host_ip = $_SERVER['REMOTE_ADDR'];
    $count = $redis->get($_SERVER['REMOTE_ADDR']);
    $self = $_SERVER['SERVER_ADDR'];
-   print "hey $host_ip, this is your $count visit on container $self!";
+   print "Hey $host_ip, this is your $count visit on container $self!";
 ?>
