f24fa30a44ed576a7fb4b908d37b225130865e68
mceith <contact@mceith.com>
2015-11-07 21:10:42 +0200

    redis-slave id

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php
-   //Connecting to Redis server on localhost
    $redis = new Redis();
-   $redis->connect('DBADDR', 6379);
-   //set the data in redis string
+   $redis->connect('172.30.140.203', 6379);
    $redis->setnx($_SERVER['REMOTE_ADDR'], 1);
-   // Get the stored data and print it
    $redis->incr($_SERVER['REMOTE_ADDR']);
-	 $host_ip = $_SERVER['REMOTE_ADDR'];
-	 $redis = new Redis();
-	 $redis->connect('redis-slave', 6379);
+         $host_ip = $_SERVER['REMOTE_ADDR'];
+         $redis = new Redis();
+         $redis->connect('redis-slave', 6379);
    $count = $redis->get($_SERVER['REMOTE_ADDR']);
    $self = $_SERVER['SERVER_ADDR'];
-	 print "Hey  $host_ip, this is your $count visit on container $self!!";
-	 echo 'Client IP: '.$_SERVER["HTTP_X_FORWARDED_FOR"];
-	 phpinfo();
-	 foreach (getallheaders() as $name => $value) {
-		     echo "$name: $value<br>\n";
-	 }
+   //phpinfo();
+   $server_info = $redis->info();
+   print "time is $cpu";
+   $array = array_values($server_info);
+   $redis_server = $array[10];
+   print "Hey  $host_ip, this is your $count visit on container $self and redis-slave $redis_server";
 ?>
