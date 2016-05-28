2b6d2769391a91a8593f6d070b5b3bd7b8a87903
mceith <contact@mceith.com>
2016-05-28 23:26:04 +0300

    Added x-forward-for header

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php
+
    function get_ip() {
-     //Just get the headers if we can or else use the SERVER global
-     if ( function_exists( 'apache_request_headers' ) ) {
-       $headers = apache_request_headers();
-     } else {
-       $headers = $_SERVER;
-     }
-     //Get the forwarded IP if it exists
-     if ( array_key_exists( 'X-Forwarded-For', $headers ) && filter_var( $headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ) {
-       $the_ip = $headers['X-Forwarded-For'];
-      } elseif ( array_key_exists( 'HTTP_X_FORWARDED_FOR', $headers ) && filter_var( $headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) {
-	  $the_ip = $headers['HTTP_X_FORWARDED_FOR'];
-      } else {
-          $the_ip = filter_var( $_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 );
-      }
-     return $the_ip;
-   }
+		//Just get the headers if we can or else use the SERVER global
+		if ( function_exists( 'apache_request_headers' ) ) {
+			$headers = apache_request_headers();
+		} else {
+			$headers = $_SERVER;
+		}
+		//Get the forwarded IP if it exists
+		if ( array_key_exists( 'X-Forwarded-For', $headers ) && filter_var( $headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ) {
+			$the_ip = $headers['X-Forwarded-For'];
+		} elseif ( array_key_exists( 'HTTP_X_FORWARDED_FOR', $headers ) && filter_var( $headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 )
+		) {
+			$the_ip = $headers['HTTP_X_FORWARDED_FOR'];
+		} else {
+			
+			$the_ip = filter_var( $_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 );
+		}
+		return $the_ip;
+	}
+
    $redis = new Redis();
    $redis->connect('DBADDR', 6379);
    $redis->setnx(get_ip(), 1);
@@ -29,5 +33,5 @@
    $server_info = $redis->info();
    $array = array_values($server_info);
    $redis_server = $array[10];
-   print "Heyy People!!  $host_ip, this is your $count visit on container $self and redis-slave $redis_server";
+   print "Hey $host_ip, this is your $count visit on container $self and redis-slave $redis_server";
 ?>
